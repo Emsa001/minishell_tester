4f53dc55da7902e86bfcaa1b2202d7355ec31a67
Emanuel Scura <escura@c2r8s3.42wolfsburg.de>
2024-04-16 17:57:16 +0200

    output updates

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
 const { spawn } = require("child_process");
 
+const SHOW_OUTPUT = (process.argv[3] || 'true') == 'true';
 const tests = "./tests";
-const minishellDir = process.argv.slice(2).join('') || "../minishell";
 const bashDir = "/bin/bash";
+
+const minishellDir = process.argv[2] || "../minishell";
+
+let testNumber = 0;
+let successNumber = 0;
 
 class Test {
     constructor(name, commands) {
@@ -37,27 +42,30 @@ class Test {
         });
     }
 
-    async showResults(bashOutput,minishellOutput){
+    async showResults(bashOutput, minishellOutput) {
         let success = 0;
         let tests = 0;
-        const bashArr = bashOutput.join("").split('cmd\n'); 
+        const bashArr = bashOutput.join("").split("cmd\n");
         let minishellArr = minishellOutput.join("").split(this.prompt);
         minishellArr.pop();
-    
-        console.log(`\n=================[ \x1b[33m${this.name}\x1b[0m ]=================\n`)
+
+        console.log(`\n=================[ \x1b[33m${this.name}\x1b[0m ]=================\n`);
 
         for (let i = 0; i < minishellArr.length; i++) {
             const miniLine = minishellArr[i].split("\n");
             const command = miniLine[0];
-            
+
             let output = miniLine.slice(1).join("\n");
             const bashOutput = bashArr[i];
 
-            if(command){
-                if(bashOutput === output){
+            if (command) {
+                if (bashOutput === output) {
+                    output = output.replace(/\n/g, " \\n ");
+                    const outputFormatted = " " + output.trim().substring(0, 50) + (output.trim().length > 50 ? "..." : "");
                     console.log(`${i}. \x1b[32m✓\x1b[0m ${command}`);
+                    if (output && SHOW_OUTPUT) console.log(`   \x1b[90m${outputFormatted}\x1b[0m`);
                     success++;
-                }else{
+                } else {
                     console.log("-----------------------------------");
                     console.log(`\x1b[31mNumber:\x1b[0m ${i}`);
                     console.log(`\x1b[31mCommand:\x1b[0m ${command}`);
@@ -69,11 +77,14 @@ class Test {
             }
         }
 
+        testNumber += tests;
+        successNumber += success;
+
         let color = success === tests ? "\x1b[32m" : "\x1b[31m";
         console.log(`\n${color}${success}/${tests} \x1b[0mtests passed\n`);
     }
 
-    async runTests(){
+    async runTests() {
         this.prompt = await this.getPrompt();
         let minishellOutput = [];
         let bashOutput = [];
@@ -93,44 +104,43 @@ class Test {
 
         bash.stdout.on("close", () => {
             bashEnded = true;
-            if(minishellEnded){
-                this.showResults(bashOutput,minishellOutput);
+            if (minishellEnded) {
+                return this.showResults(bashOutput, minishellOutput);
             }
         });
 
         minishell.stdout.on("close", () => {
             minishellEnded = true;
-            if(bashEnded){
-                this.showResults(bashOutput,minishellOutput);
+            if (bashEnded) {
+                return this.showResults(bashOutput, minishellOutput);
             }
         });
 
-        for(const command of this.commands){
+        for (const command of this.commands) {
             minishell.stdin.write(`${command}\n`);
             bash.stdin.write(`echo cmd\n`);
             bash.stdin.write(`${command}\n`);
         }
         bash.stdin.end();
         minishell.stdin.end();
-    }   
+    }
 }
-
 
 async function runSets() {
     try {
         const files = await fs.promises.readdir(tests);
-        for (const file of files) {
-            try {
-                if(!file.startsWith("_")){
+        const testsPromises = files
+            .filter((file) => !file.startsWith("_"))
+            .map(async (file) => {
+                try {
                     const data = await fs.promises.readFile(`${tests}/${file}`, "utf8");
                     const commands = data.split("\n").filter((line) => line.trim() !== "");
                     const test = new Test(file, commands);
-                    test.runTests();
+                    return test.runTests();
+                } catch (error) {
+                    console.error(`Error reading file ${file}: ${error}`);
                 }
-            } catch (error) {
-                console.error(`Error reading file ${file}: ${error}`);
-            }
-        }
+            });
     } catch (error) {
         console.error("Error reading tests directory:", error);
     }
