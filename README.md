dd1dea206da3385aa11b47094fa8d86e0eebf5a3
Emanuel Scura <escura@c2r8s3.42wolfsburg.de>
2024-04-16 17:03:56 +0200

    fixes + readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Please note that this project is currently under development and not yet fully functional. I strongly advise against using it as a final minishell tester.
+# Project Name
+
+> Please note: This project is currently under development and is not yet fully functional. It is not recommended to use it as a final minishell tester.
+
+## Installation
+
+nodejs required
+
+## Usage
+
+To use this project, follow the steps below:
+
+1. Place your tests in the `./tests` directory.
+2. If you want to disable a test, add an underscore (`_`) to the beginning of its name. For example, to disable a test named `test1`, rename it to `_test1`.
+3. Run the command `node index.js <PATH_TO_MINISHELL>`
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ class Test {
     constructor(name, commands) {
         this.name = name;
         this.commands = commands;
+        this.prompt = null;
     }
 
     async getPrompt() {
@@ -36,13 +37,49 @@ class Test {
         });
     }
 
-    async runTests(){
+    async showResults(bashOutput,minishellOutput){
         let success = 0;
         let tests = 0;
+        const bashArr = bashOutput.join("").split('cmd\n'); 
+        let minishellArr = minishellOutput.join("").split(this.prompt);
+        minishellArr.pop();
+    
+        console.log(`\n=================[ \x1b[33m${this.name}\x1b[0m ]=================\n`)
 
-        const PROMPT = await this.getPrompt();
+        for (let i = 0; i < minishellArr.length; i++) {
+            const miniLine = minishellArr[i].split("\n");
+            const command = miniLine[0];
+            
+            let output = miniLine.slice(1).join("\n");
+            const bashOutput = bashArr[i];
+
+            if(command){
+                if(bashOutput === output){
+                    console.log(`${i}. \x1b[32m✓\x1b[0m ${command}`);
+                    success++;
+                }else{
+                    console.log("-----------------------------------");
+                    console.log(`\x1b[31mNumber:\x1b[0m ${i}`);
+                    console.log(`\x1b[31mCommand:\x1b[0m ${command}`);
+                    console.log(`\x1b[31mExpected:\x1b[0m ${bashOutput}`);
+                    console.log(`\x1b[31mGot:\x1b[0m ${output}`);
+                    console.log("-----------------------------------");
+                }
+                tests++;
+            }
+        }
+
+        let color = success === tests ? "\x1b[32m" : "\x1b[31m";
+        console.log(`\n${color}${success}/${tests} \x1b[0mtests passed\n`);
+    }
+
+    async runTests(){
+        this.prompt = await this.getPrompt();
         let minishellOutput = [];
         let bashOutput = [];
+
+        let bashEnded = false;
+        let minishellEnded = false;
 
         const minishell = spawn(minishellDir);
         const bash = spawn(bashDir);
@@ -54,37 +91,18 @@ class Test {
             bashOutput.push(data.toString());
         });
 
-        minishell.stderr.on("close", () => {
-            const bashArr = bashOutput.join("").split('cmd\n'); 
-            let minishellArr = minishellOutput.join("").split(PROMPT);
-            minishellArr.pop();
-        
-            console.log(`\n=================[ \x1b[33m${this.name}\x1b[0m ]=================\n`)
-
-            for (let i = 0; i < minishellArr.length; i++) {
-                const miniLine = minishellArr[i].split("\n");
-                const command = miniLine[0];
-                
-                let output = miniLine.slice(1).join("\n");
-                const bashOutput = bashArr[i];
-
-                if(command){
-                    if(bashOutput === output){
-                        console.log(`\x1b[32m✓\x1b[0m ${command}`);
-                        success++;
-                    }else{
-                        console.log("-----------------------------------");
-                        console.log(`\x1b[31mCommand:\x1b[0m ${command}`);
-                        console.log(`\x1b[31mExpected:\x1b[0m ${bashOutput}`);
-                        console.log(`\x1b[31mGot:\x1b[0m ${output}`);
-                        console.log("-----------------------------------");
-                    }
-                    tests++;
-                }
+        bash.stdout.on("close", () => {
+            bashEnded = true;
+            if(minishellEnded){
+                this.showResults(bashOutput,minishellOutput);
             }
+        });
 
-            let color = success === tests ? "\x1b[32m" : "\x1b[31m";
-            console.log(`\n${color}${success}/${tests} \x1b[0mtests passed\n`);
+        minishell.stdout.on("close", () => {
+            minishellEnded = true;
+            if(bashEnded){
+                this.showResults(bashOutput,minishellOutput);
+            }
         });
 
         for(const command of this.commands){
@@ -93,9 +111,7 @@ class Test {
             bash.stdin.write(`${command}\n`);
         }
         bash.stdin.end();
-        setTimeout(() => {
-            minishell.stdin.end();
-        }, 10);
+        minishell.stdin.end();
     }   
 }
 
@@ -105,10 +121,12 @@ async function runSets() {
         const files = await fs.promises.readdir(tests);
         for (const file of files) {
             try {
-                const data = await fs.promises.readFile(`${tests}/${file}`, "utf8");
-                const commands = data.split("\n").filter((line) => line.trim() !== "");
-                const test = new Test(file, commands);
-                test.runTests();
+                if(!file.startsWith("_")){
+                    const data = await fs.promises.readFile(`${tests}/${file}`, "utf8");
+                    const commands = data.split("\n").filter((line) => line.trim() !== "");
+                    const test = new Test(file, commands);
+                    test.runTests();
+                }
             } catch (error) {
                 console.error(`Error reading file ${file}: ${error}`);
             }
